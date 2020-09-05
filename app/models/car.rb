b63e199411d0c8b712732fefb9551bda4ecfb0b0
Salvador <salvador.on@outlook.com>
2020-09-05 19:19:05 -0300

    fix-method computer car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -7,22 +7,22 @@ class Car < ApplicationRecord
   before_save :robot_guard, unless: :new_record?
 
   def robot_guard
-    return unless Car.computer_scan_failure(self)
+    return unless self.computer_scan_failure
 
-    defective_part = Car.computer_defective_part(self)
+    defective_part = self.computer_defective_part
     self.status = 'Scrap'
     self.stage = 'Factory_stock'
     self.stock_id = '1'
     SlackWorker.perform("*Scrap detected*\n *carId:* #{self.id} \n *Defective part:* #{defective_part} ")
   end
 
-  def self.computer_scan_failure(car)
-    [car.chassis, car.wheels, car.lasser, car.computer, car.engine, car.seat].include?(false)
+  def computer_scan_failure
+    [self.chassis, self.wheels, self.lasser, self.computer, self.engine, self.seat].include?(false)
   end
 
-  def self.computer_defective_part(car)
+  def computer_defective_part
     parts = %w[chassis wheels lasser computer engine seat]
-    parts_list = Hash[parts.map { |part| [part, car[part]] }]
+    parts_list = Hash[parts.map { |part| [part, self[part]] }]
     parts_list.key(false)
   end
 end
